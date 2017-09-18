37fb3baa2f1a4897a873a6012cbc6c8ec9d0f206
Xaioting <ting02280@gmail.com>
2017-09-18 15:12:42 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+	 System.out.println("學號：106022220");
+        System.out.println("系級：行動商務與多媒體應用學系一年級");
+        System.out.println("姓名：黃筱婷");
+        System.out.println("時間：2017/09/18");
+        System.out.print("地點：\"I319\"");
+
+    }
+}
